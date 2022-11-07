9bcb6bb90b46c3543f3ceac93ccba8f1290c77dd
NaitsirhcEmorej07 <naitsirhcemorej@gmail.com>
2022-11-07 10:15:56 +0800

    UNCOMMENT ECHO

--- modals/add-items-modal.php
+++ modals/add-items-modal.php
@@ -36,8 +36,8 @@ if (isset($_POST['submitBT'])) {
     UPPER('$received_from'), 
     UPPER('$supplier_contact'),
     UPPER($supplier_warranty) )";
-    // $conn->query($sql) or die($conn->error);
-    echo($sql);
+    $conn->query($sql) or die($conn->error);
+    // echo($sql);
 
     if ($conn) {
         header('Location: inventory-items.php');
