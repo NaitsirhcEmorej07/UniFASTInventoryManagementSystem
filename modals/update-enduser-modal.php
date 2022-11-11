2810c1a7e553958c791c731a1ad236e818ee1924
NaitsirhcEmorej07 <naitsirhcemorej@gmail.com>
2022-11-11 11:09:18 +0800

    END USER MODAL WITH N/A BUTTON

--- modals/update-enduser-modal.php
+++ modals/update-enduser-modal.php
@@ -60,7 +60,11 @@ $result = $conn->query($sql);
                                         <path d="M8 1a2 2 0 0 1 2 2v2H6V3a2 2 0 0 1 2-2zm3 4V3a3 3 0 1 0-6 0v2H3.36a1.5 1.5 0 0 0-1.483 1.277L.85 13.13A2.5 2.5 0 0 0 3.322 16h9.355a2.5 2.5 0 0 0 2.473-2.87l-1.028-6.853A1.5 1.5 0 0 0 12.64 5H11zm-1 1v1.5a.5.5 0 0 0 1 0V6h1.639a.5.5 0 0 1 .494.426l1.028 6.851A1.5 1.5 0 0 1 12.678 15H3.322a1.5 1.5 0 0 1-1.483-1.723l1.028-6.851A.5.5 0 0 1 3.36 6H5v1.5a.5.5 0 1 0 1 0V6h4z" />
                                     </svg>
                                     Serial Number</label>
-                                <input type="text" name="serial_number" id="serial_number" class="form-control" autocomplete="off" required>
+                                <div class="d-flex justify-content-left">
+                                    <input type="text" name="serial_number" id="serial_number" class="form-control" autocomplete="off" style="width: 190px; display:flex;" required>
+                                    <div id="generateserial" class="generateserial btn btn-secondary" style="padding:2px; width: 32px; font-size:14px; font-weight:500; text-align: left;">N/A</div>
+                                </div>
+
                             </div>
 
                             <div class="col-6 mb-3">
